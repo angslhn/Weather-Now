37d0c39e86132459c625ab2d789d9e4c770c25ab
angslhn <angslhn@gmail.com>
2025-02-22 10:38:29 +0700

    Weather Now.

--- src/layouts/check/index.jsx
+++ src/layouts/check/index.jsx
@@ -5,15 +5,22 @@ import Header from "../../page/header";
 import Main from "../../page/main/check";
 import Footer from "../../page/footer";
 
+import { Alert } from '@mui/material';
+
 const Home = () => {
   const [data, setData] = useState(null);
   const [imageSource, setImageSource] = useState(null);
   const [location, setLocation] = useState(null);
   const [loading, setLoading] = useState({ weather: true, image: true })
+  const [checkingLocation, setCheckingLocation] = useState(true);
+  const [locationAccess, setLocationAccess] = useState(true)
   
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
+        setLocationAccess(true)
+        setCheckingLocation(false)
+
         async function fetchWeatherData() {
           try {
             setLoading((prev) => ({...prev, weather: true}))
@@ -40,7 +47,8 @@ const Home = () => {
         fetchWeatherData();
       },
       () => {
-        console.log("Gagal Mendapatkan Lokasi");
+        setLocationAccess(false);
+        setCheckingLocation(false)
       },
       {
         enableHighAccuracy: true,
@@ -51,7 +59,7 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    if (!data) return
+    if (!data) return;
 
     async function fetchImageWeatherSource() {
       try {
@@ -106,9 +114,7 @@ const Home = () => {
     <>
       <Header />
       {
-        (!loading.weather && !loading.image) ?
-          <Main weather={data} image={imageSource} />
-        :
+        checkingLocation ?
           <div role="status">
             <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
@@ -116,6 +122,12 @@ const Home = () => {
             </svg>
             <span className="sr-only">Loading...</span>
           </div>
+        : !locationAccess ?
+          <Alert variant='filled' severity="error">
+            Oops! Unable to get location access at this time.
+          </Alert>
+        : (!loading.weather && !loading.image) &&
+          <Main weather={data} image={imageSource} />
         }
       <Footer />
     </>
--- src/page/main/check/index.jsx
+++ src/page/main/check/index.jsx
@@ -23,7 +23,7 @@ const Main = ({ weather, image }) => {
                         </span>
                     </div>
                     <div className="weathernow__check__information__weather__icon">
-                        <img src={ imageSource?.[String(data?.current?.condition?.code)] || imageSource?.["1030"] } alt=""/>
+                        <img src={ imageSource?.[String(data?.current?.condition?.code)] } alt="Icon Not Set."/>
                     </div>
                 </div>
                 <div className="weathernow__check__information__other">
